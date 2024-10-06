e264256547ffc0f213129f27d8c8a6cde579d63c
Themaire <n.elie@me.com>
2024-10-06 11:18:20 +0200

    Update add_aliases.sh

--- add_aliases.sh
+++ add_aliases.sh
@@ -2,9 +2,9 @@
 
 # Auto adding the aliases to the end of your ~/.barshrc config file.
 
-if [ ! -e "~/bash_aliases" ] # Test if this forlder exists.
+if [ ! -e "~/.bash_aliases" ] # Test if this forlder exists.
 then
-  echo "Create ~/bash_aliases folder."
+  echo "Create ~/.bash_aliases folder."
   mkdir ~/bash_aliases
 
   for i in ./*aliases
